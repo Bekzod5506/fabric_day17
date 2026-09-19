f0fa4f46929d781b54e3ab6ddb45ad4ffa90b6e1
Bekzod <abdumurodovbekzod7@gmail.com>
2026-09-19 11:01:12 +0500

    Committing 1 item from workspace 4031375f-1d22-4362-ab6f-14d47907152e

--- FabricItems/day17_Notebook.Notebook/notebook-content.py
+++ FabricItems/day17_Notebook.Notebook/notebook-content.py
@@ -6,14 +6,25 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "5c6d4568-40dd-4710-bd74-b23d9deb112e",
+# META       "default_lakehouse_name": "day15_lakehouse",
+# META       "default_lakehouse_workspace_id": "4031375f-1d22-4362-ab6f-14d47907152e",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "5c6d4568-40dd-4710-bd74-b23d9deb112e"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
-
+print("Github")
 
 # METADATA ********************
 
